6bf02682953ee4e2ba6080a7245e933dad173fc2
Luis Herranz <luisherranz@gmail.com>
2017-01-12 17:47:21 +0100

    Save posts to state (WIP).

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
+import { normalize, schema } from 'normalizr';
+import * as types from '../types';
 
-export const test = () => false;
+export const posts = (state = [], action) => {
+  if (action.type === types.REFRESH_POSTS_SUCCEED) {
+    normalize;
+    schema;
+    debugger;
+    return action.posts;
+  }
+  return state;
+};
 
 export default () => combineReducers({
-  test,
+  posts,
 });
