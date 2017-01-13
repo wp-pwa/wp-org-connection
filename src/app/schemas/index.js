f50656971140df918844606335356efc2b037519
Luis Herranz <luisherranz@gmail.com>
2017-01-13 17:27:37 +0100

    Add normalized reducer of categories.

--- src/app/schemas/index.js
+++ src/app/schemas/index.js
@@ -1,18 +1,7 @@
-import { schema } from 'normalizr';
+import posts from './posts';
+import categories from './categories';
 
-const author = new schema.Entity('author');
-const featuredMedia = new schema.Entity('featured_media');
-const category = new schema.Entity('categories');
-const categories = new schema.Array(category);
-const tag = new schema.Entity('tags');
-const tags = new schema.Array(tag);
-const term = new schema.Array({
-  category: categories,
-  post_tag: tags,
-}, input => input[0] && input[0].taxonomy);
-const post = new schema.Entity('post', { _embedded: {
-  author: [author],
-  'wp:featuredmedia': [featuredMedia],
-  'wp:term': term,
-} });
-export const posts = new schema.Array(post);
+export {
+  posts,
+  categories,
+};
