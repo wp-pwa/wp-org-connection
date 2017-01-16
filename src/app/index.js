b6be1a0ae7ba98e863f177598b87b44ed70aeb9c
Luis Herranz <luisherranz@gmail.com>
2017-01-16 18:37:42 +0100

    Add actions, types and selectors to index.

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,15 @@
+import * as actions from './actions';
+import * as types from './types';
 import * as sagas from './sagas';
 import * as reducers from './reducers';
+import * as selectors from './selectors';
 import * as deps from './deps';
 
 export {
+  actions,
+  types,
   sagas,
   reducers,
+  selectors,
   deps,
 };
