f58518ccde5e1c5b3548a61f7b5b4fb1254a86c6
Luis Herranz <luisherranz@gmail.com>
2017-01-16 17:19:51 +0100

    Add newCategorySelected action.

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,21 +1,15 @@
 import * as types from '../types';
 
-export const refreshPostsRequested = () => ({
-  type: types.REFRESH_POSTS_REQUESTED,
-});
-export const refreshPostsSucceed = ({ posts }) => ({
-  type: types.REFRESH_POSTS_SUCCEED, posts,
-});
-export const refreshPostsFailed = ({ error }) => ({
-  type: types.REFRESH_POSTS_FAILED, error,
-});
+export const refreshPostsRequested = (action = {}) =>
+  ({ type: types.REFRESH_POSTS_REQUESTED, params: action.params });
+export const refreshPostsSucceed = ({ posts }) => ({ type: types.REFRESH_POSTS_SUCCEED, posts });
+export const refreshPostsFailed = ({ error }) => ({ type: types.REFRESH_POSTS_FAILED, error });
 
-export const refreshCategoriesRequested = () => ({
-  type: types.REFRESH_CATEGORIES_REQUESTED,
-});
-export const refreshCategoriesSucceed = ({ categories }) => ({
-  type: types.REFRESH_CATEGORIES_SUCCEED, categories,
-});
-export const refreshCategoriesFailed = ({ error }) => ({
-  type: types.REFRESH_CATEGORIES_FAILED, error,
-});
+export const refreshCategoriesRequested = () => ({ type: types.REFRESH_CATEGORIES_REQUESTED });
+export const refreshCategoriesSucceed = ({ categories }) =>
+  ({ type: types.REFRESH_CATEGORIES_SUCCEED, categories });
+export const refreshCategoriesFailed = ({ error }) =>
+  ({ type: types.REFRESH_CATEGORIES_FAILED, error });
+
+export const newCategorySelected = ({ category }) =>
+  ({ type: types.NEW_CATEGORY_SELECTED, category });
