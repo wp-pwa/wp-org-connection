8e65c41d762fe654a7cd9516a4d83a12628cf168
Luis Herranz <luisherranz@gmail.com>
2017-01-19 17:32:22 +0100

    Define new actions and types.

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,15 +1,103 @@
 import * as types from '../types';
 
-export const refreshPostsRequested = () => ({ type: types.REFRESH_POSTS_REQUESTED });
-export const refreshPostsSucceed = ({ posts, params }) =>
-  ({ type: types.REFRESH_POSTS_SUCCEED, posts, params });
-export const refreshPostsFailed = ({ error, params }) =>
-  ({ type: types.REFRESH_POSTS_FAILED, error, params });
+export const globalParamsChanged = ({ params, options }) =>
+  ({ type: types.GLOBAL_PARAMS_CHANGED, params, options });
 
-export const refreshCategoriesRequested = () => ({ type: types.REFRESH_CATEGORIES_REQUESTED });
-export const refreshCategoriesSucceed = ({ categories }) =>
-  ({ type: types.REFRESH_CATEGORIES_SUCCEED, categories });
-export const refreshCategoriesFailed = ({ error }) =>
-  ({ type: types.REFRESH_CATEGORIES_FAILED, error });
+export const postsListRequested = ({ params }) => ({ type: types.POSTS_LIST_REQUESTED, params });
+export const postsListSucceed = ({ params, posts }) =>
+  ({ type: types.POSTS_LIST_SUCCEED, params, posts });
+export const postsListFailed = ({ params, error }) =>
+  ({ type: types.POSTS_LIST_FAILED, params, error });
+export const postRequested = ({ id }) => ({ type: types.POST_REQUESTED, id });
+export const postSucceed = ({ id, post }) => ({ type: types.POST_SUCCEED, id, post });
+export const postFailed = ({ id, error }) => ({ type: types.POST_FAILED, id, error });
 
-export const postParamsChanged = ({ params }) => ({ type: types.POST_PARAMS_CHANGED, params });
+export const pagesListRequested = ({ params }) => ({ type: types.PAGES_LIST_REQUESTED, params });
+export const pagesListSucceed = ({ params, pages }) =>
+  ({ type: types.PAGES_LIST_SUCCEED, params, pages });
+export const pagesListFailed = ({ params, error }) =>
+  ({ type: types.PAGES_LIST_FAILED, params, error });
+export const pageRequested = ({ id }) => ({ type: types.PAGE_REQUESTED, id });
+export const pageSucceed = ({ id, page }) => ({ type: types.PAGE_SUCCEED, id, page });
+export const pageFailed = ({ id, error }) => ({ type: types.PAGE_FAILED, id, error });
+
+export const tagsListRequested = ({ params }) => ({ type: types.TAGS_LIST_REQUESTED, params });
+export const tagsListSucceed = ({ params, tags }) =>
+  ({ type: types.TAGS_LIST_SUCCEED, params, tags });
+export const tagsListFailed = ({ params, error }) =>
+  ({ type: types.TAGS_LIST_FAILED, params, error });
+export const tagRequested = ({ id }) => ({ type: types.TAG_REQUESTED, id });
+export const tagSucceed = ({ id, tag }) => ({ type: types.TAG_SUCCEED, id, tag });
+export const tagFailed = ({ id, error }) => ({ type: types.TAG_FAILED, id, error });
+
+export const usersListRequested = ({ params }) => ({ type: types.USERS_LIST_REQUESTED, params });
+export const usersListSucceed = ({ params, users }) =>
+  ({ type: types.USERS_LIST_SUCCEED, params, users });
+export const usersListFailed = ({ params, error }) =>
+  ({ type: types.USERS_LIST_FAILED, params, error });
+export const userRequested = ({ id }) => ({ type: types.USER_REQUESTED, id });
+export const userSucceed = ({ id, user }) => ({ type: types.USER_SUCCEED, id, user });
+export const userFailed = ({ id, error }) => ({ type: types.USER_FAILED, id, error });
+
+export const mediaListRequested = ({ params }) => ({ type: types.MEDIA_LIST_REQUESTED, params });
+export const mediaListSucceed = ({ params, media }) =>
+  ({ type: types.MEDIA_LIST_SUCCEED, params, media });
+export const mediaListFailed = ({ params, error }) =>
+  ({ type: types.MEDIA_LIST_FAILED, params, error });
+export const mediaRequested = ({ id }) => ({ type: types.MEDIA_REQUESTED, id });
+export const mediaSucceed = ({ id, media }) => ({ type: types.MEDIA_SUCCEED, id, media });
+export const mediaFailed = ({ id, error }) => ({ type: types.MEDIA_FAILED, id, error });
+
+export const categoriesListRequested = ({ params }) =>
+  ({ type: types.CATEGORIES_LIST_REQUESTED, params });
+export const categoriesListSucceed = ({ params, categories }) =>
+  ({ type: types.CATEGORIES_LIST_SUCCEED, params, categories });
+export const categoriesListFailed = ({ params, error }) =>
+  ({ type: types.CATEGORIES_LIST_FAILED, params, error });
+export const categoryRequested = ({ id }) => ({ type: types.CATEGORY_REQUESTED, id });
+export const categorySucceed = ({ id, category }) =>
+  ({ type: types.CATEGORY_SUCCEED, id, category });
+export const categoryFailed = ({ id, error }) => ({ type: types.CATEGORY_FAILED, id, error });
+
+export const commentsListRequested = ({ params }) =>
+  ({ type: types.COMMENTS_LIST_REQUESTED, params });
+export const commentsListSucceed = ({ params, comments }) =>
+  ({ type: types.COMMENTS_LIST_SUCCEED, params, comments });
+export const commentsListFailed = ({ params, error }) =>
+  ({ type: types.COMMENTS_LIST_FAILED, params, error });
+export const commentRequested = ({ id }) => ({ type: types.COMMENT_REQUESTED, id });
+export const commentSucceed = ({ id, comment }) => ({ type: types.COMMENT_SUCCEED, id, comment });
+export const commentFailed = ({ id, error }) => ({ type: types.COMMENT_FAILED, id, error });
+
+export const taxonomiesListRequested = ({ params }) =>
+  ({ type: types.TAXONOMIES_LIST_REQUESTED, params });
+export const taxonomiesListSucceed = ({ params, taxonomies }) =>
+  ({ type: types.TAXONOMIES_LIST_SUCCEED, params, taxonomies });
+export const taxonomiesListFailed = ({ params, error }) =>
+  ({ type: types.TAXONOMIES_LIST_FAILED, params, error });
+export const taxonomyRequested = ({ id }) => ({ type: types.TAXONOMY_REQUESTED, id });
+export const taxonomySucceed = ({ id, taxonomy }) =>
+  ({ type: types.TAXONOMY_SUCCEED, id, taxonomy });
+export const taxonomyFailed = ({ id, error }) => ({ type: types.TAXONOMY_FAILED, id, error });
+
+export const postTypesListRequested = ({ params }) =>
+  ({ type: types.POST_TYPES_LIST_REQUESTED, params });
+export const postTypesListSucceed = ({ params, postTypes }) =>
+  ({ type: types.POST_TYPES_LIST_SUCCEED, params, postTypes });
+export const postTypesListFailed = ({ params, error }) =>
+  ({ type: types.POST_TYPES_LIST_FAILED, params, error });
+export const postTypeRequested = ({ id }) => ({ type: types.POST_TYPE_REQUESTED, id });
+export const postTypeSucceed = ({ id, postType }) =>
+  ({ type: types.POST_TYPE_SUCCEED, id, postType });
+export const postTypeFailed = ({ id, error }) => ({ type: types.POST_TYPE_FAILED, id, error });
+
+export const postStatusesListRequested = ({ params }) =>
+  ({ type: types.POST_STATUSES_LIST_REQUESTED, params });
+export const postStatusesListSucceed = ({ params, postStatuses }) =>
+  ({ type: types.POST_STATUSES_LIST_SUCCEED, params, postStatuses });
+export const postStatusesListFailed = ({ params, error }) =>
+  ({ type: types.POST_STATUSES_LIST_FAILED, params, error });
+export const postStatusRequested = ({ id }) => ({ type: types.POST_STATUS_REQUESTED, id });
+export const postStatusSucceed = ({ id, postStatus }) =>
+  ({ type: types.POST_STATUS_SUCCEED, id, postStatus });
+export const postStatusFailed = ({ id, error }) => ({ type: types.POST_STATUS_FAILED, id, error });
--- src/app/types/index.js
+++ src/app/types/index.js
@@ -1,9 +1,71 @@
-export const REFRESH_POSTS_REQUESTED = 'connection/REFRESH_POSTS_REQUESTED';
-export const REFRESH_POSTS_FAILED = 'connection/REFRESH_POSTS_FAILED';
-export const REFRESH_POSTS_SUCCEED = 'connection/REFRESH_POSTS_SUCCEED';
+export const GLOBAL_PARAMS_CHANGED = 'connection/GLOBAL_PARAMS_CHANGED';
 
-export const REFRESH_CATEGORIES_REQUESTED = 'connection/REFRESH_CATEGORIES_REQUESTED';
-export const REFRESH_CATEGORIES_FAILED = 'connection/REFRESH_CATEGORIES_FAILED';
-export const REFRESH_CATEGORIES_SUCCEED = 'connection/REFRESH_CATEGORIES_SUCCEED';
+export const POSTS_LIST_REQUESTED = 'connection/POSTS_LIST_REQUESTED';
+export const POSTS_LIST_SUCCEED = 'connection/POSTS_LIST_SUCCEED';
+export const POSTS_LIST_FAILED = 'connection/POSTS_LIST_FAILED';
+export const POST_REQUESTED = 'connection/POST_REQUESTED';
+export const POST_SUCCEED = 'connection/POST_SUCCEED';
+export const POST_FAILED = 'connection/POST_FAILED';
 
-export const POST_PARAMS_CHANGED = 'connection/POST_PARAMS_CHANGED';
+export const PAGES_LIST_REQUESTED = 'connection/PAGES_LIST_REQUESTED';
+export const PAGES_LIST_SUCCEED = 'connection/PAGES_LIST_SUCCEED';
+export const PAGES_LIST_FAILED = 'connection/PAGES_LIST_FAILED';
+export const PAGE_REQUESTED = 'connection/PAGE_REQUESTED';
+export const PAGE_SUCCEED = 'connection/PAGE_SUCCEED';
+export const PAGE_FAILED = 'connection/PAGE_FAILED';
+
+export const TAGS_LIST_REQUESTED = 'connection/TAGS_LIST_REQUESTED';
+export const TAGS_LIST_SUCCEED = 'connection/TAGS_LIST_SUCCEED';
+export const TAGS_LIST_FAILED = 'connection/TAGS_LIST_FAILED';
+export const TAG_REQUESTED = 'connection/TAG_REQUESTED';
+export const TAG_SUCCEED = 'connection/TAG_SUCCEED';
+export const TAG_FAILED = 'connection/TAG_FAILED';
+
+export const USERS_LIST_REQUESTED = 'connection/USERS_LIST_REQUESTED';
+export const USERS_LIST_SUCCEED = 'connection/USERS_LIST_SUCCEED';
+export const USERS_LIST_FAILED = 'connection/USERS_LIST_FAILED';
+export const USER_REQUESTED = 'connection/USER_REQUESTED';
+export const USER_SUCCEED = 'connection/USER_SUCCEED';
+export const USER_FAILED = 'connection/USER_FAILED';
+
+export const MEDIA_LIST_REQUESTED = 'connection/MEDIA_LIST_REQUESTED';
+export const MEDIA_LIST_SUCCEED = 'connection/MEDIA_LIST_SUCCEED';
+export const MEDIA_LIST_FAILED = 'connection/MEDIA_LIST_FAILED';
+export const MEDIA_REQUESTED = 'connection/MEDIA_REQUESTED';
+export const MEDIA_SUCCEED = 'connection/MEDIA_SUCCEED';
+export const MEDIA_FAILED = 'connection/MEDIA_FAILED';
+
+export const CATEGORIES_LIST_REQUESTED = 'connection/CATEGORIES_LIST_REQUESTED';
+export const CATEGORIES_LIST_SUCCEED = 'connection/CATEGORIES_LIST_SUCCEED';
+export const CATEGORIES_LIST_FAILED = 'connection/CATEGORIES_LIST_FAILED';
+export const CATEGORY_REQUESTED = 'connection/CATEGORY_REQUESTED';
+export const CATEGORY_SUCCEED = 'connection/CATEGORY_SUCCEED';
+export const CATEGORY_FAILED = 'connection/CATEGORY_FAILED';
+
+export const COMMENTS_LIST_REQUESTED = 'connection/COMMENTS_LIST_REQUESTED';
+export const COMMENTS_LIST_SUCCEED = 'connection/COMMENTS_LIST_SUCCEED';
+export const COMMENTS_LIST_FAILED = 'connection/COMMENTS_LIST_FAILED';
+export const COMMENT_REQUESTED = 'connection/COMMENT_REQUESTED';
+export const COMMENT_SUCCEED = 'connection/COMMENT_SUCCEED';
+export const COMMENT_FAILED = 'connection/COMMENT_FAILED';
+
+export const TAXONOMIES_LIST_REQUESTED = 'connection/TAXONOMIES_LIST_REQUESTED';
+export const TAXONOMIES_LIST_SUCCEED = 'connection/TAXONOMIES_LIST_SUCCEED';
+export const TAXONOMIES_LIST_FAILED = 'connection/TAXONOMIES_LIST_FAILED';
+export const TAXONOMY_REQUESTED = 'connection/TAXONOMY_REQUESTED';
+export const TAXONOMY_SUCCEED = 'connection/TAXONOMY_SUCCEED';
+export const TAXONOMY_FAILED = 'connection/TAXONOMY_FAILED';
+
+export const POST_TYPES_LIST_REQUESTED = 'connection/POST_TYPES_LIST_REQUESTED';
+export const POST_TYPES_LIST_SUCCEED = 'connection/POST_TYPES_LIST_SUCCEED';
+export const POST_TYPES_LIST_FAILED = 'connection/POST_TYPES_LIST_FAILED';
+export const POST_TYPE_REQUESTED = 'connection/POST_TYPE_REQUESTED';
+export const POST_TYPE_SUCCEED = 'connection/POST_TYPE_SUCCEED';
+export const POST_TYPE_FAILED = 'connection/POST_TYPE_FAILED';
+
+export const POST_STATUSES_LIST_REQUESTED = 'connection/POST_STATUSES_LIST_REQUESTED';
+export const POST_STATUSES_LIST_SUCCEED = 'connection/POST_STATUSES_LIST_SUCCEED';
+export const POST_STATUSES_LIST_FAILED = 'connection/POST_STATUSES_LIST_FAILED';
+export const POST_STATUS_REQUESTED = 'connection/POST_STATUS_REQUESTED';
+export const POST_STATUS_SUCCEED = 'connection/POST_STATUS_SUCCEED';
+export const POST_STATUS_FAILED = 'connection/POST_STATUS_FAILED';
