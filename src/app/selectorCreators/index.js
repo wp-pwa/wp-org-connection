aeec458ec887dfbd6bac9d25bb2f0fdeb4cc3f68
Luis Herranz <luisherranz@gmail.com>
2017-01-20 18:13:51 +0100

    Add all selectorCreators byId.

--- src/app/selectorCreators/index.js
+++ src/app/selectorCreators/index.js
@@ -1,7 +1,12 @@
-export const getPostById = id => state => state.connection.posts.entities.posts[id];
-export const getTagById = id => state => state.connection.posts.entities.tags[id];
-export const getAuthorById = id => state => state.connection.posts.entities.author[id];
-export const getFeaturedMediaById = id =>
-  state => state.connection.posts.entities.featured_media[id];
-export const getCategoryById = id => state => state.connection.categories.entities.category[id];
 export const getParams = type => state => state.connection.params[type];
+
+export const getPostsById = id => state => state.connection.entities.posts[id];
+export const getPagesById = id => state => state.connection.entities.pages[id];
+export const getTagsById = id => state => state.connection.entities.tags[id];
+export const getUsersById = id => state => state.connection.entities.users[id];
+export const getMediaById = id => state => state.connection.entities.media[id];
+export const getCategoriesById = id => state => state.connection.entities.categories[id];
+export const getCommentsById = id => state => state.connection.entities.comments[id];
+export const getTaxonomiesById = id => state => state.connection.entities.taxonomies[id];
+export const getPostTypesById = id => state => state.connection.entities.postTypes[id];
+export const getPostStatusesById = id => state => state.connection.entities.postStatuses[id];
