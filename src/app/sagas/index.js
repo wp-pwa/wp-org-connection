85b6468ae4a8e7a5ef1efdf059ead31eb7615b6c
Luis Herranz <luisherranz@gmail.com>
2017-04-11 12:54:26 +0200

    Change cors-worona by backend-cors

--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -14,7 +14,7 @@ import * as schemas from '../schemas';
 import * as deps from '../deps';
 import { wpTypesPlural, wpTypesSingular, wpTypesSingularToPlural } from '../constants';
 
-const CorsAnywhere = 'https://cors.worona.io/';
+const CorsAnywhere = 'https://backend.worona.io/api/v1/cors/';
 
 const getList = ({ connection, wpType, params, page }) => {
   let query = connection[wpType]().page(page);
