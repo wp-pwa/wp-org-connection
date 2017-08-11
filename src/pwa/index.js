3124140220aea4094f08b650bce7d78b303f17f5
Luis Herranz <luisherranz@gmail.com>
2017-08-11 18:18:23 +0200

    Add client and server sagas.

--- src/pwa/index.js
+++ src/pwa/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
 import { setStatic, compose } from 'recompose';
-// import actions from './actions';
-// import types from './types';
-// import sagas from './sagas';
+import * as actions from './actions';
+import * as types from './types';
+import sagas from './sagas/client';
 import reducers from './reducers';
 // import selectors from './selectors';
 // import selectorCreators from './selectorCreators';
@@ -10,9 +9,9 @@ import reducers from './reducers';
 const WpOrgConnection = () => null;
 
 export default compose(
-  // setStatic('actions', actions),
-  // setStatic('types', types),
-  // setStatic('sagas', sagas),
+  setStatic('actions', actions),
+  setStatic('types', types),
+  setStatic('sagas', sagas),
   setStatic('reducers', reducers),
   // setStatic('selectors', selectors),
   // setStatic('selectorCreators', selectorCreators),
