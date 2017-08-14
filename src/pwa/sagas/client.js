fb087f71bdb08bdaac87f1ee2cdc54b91c560ade
Luis Herranz <luisherranz@gmail.com>
2017-08-14 18:24:56 +0200

    Start watching router events to retrieve content in client.

--- src/pwa/sagas/client.js
+++ src/pwa/sagas/client.js
@@ -1,3 +1,12 @@
+import { takeEvery, all, fork } from 'redux-saga/effects';
+import { dep } from 'worona-deps';
+import wpApiWatchers from './wp-api-watchers';
+import { requestCurrentContent } from './current';
+
 export default function* () {
-  console.log('hi from wp-org connection from client');
+  const ROUTE_CHANGE_SUCCEED = dep('router', 'types', 'ROUTE_CHANGE_SUCCEED');
+  yield all([
+    fork(wpApiWatchers),
+    takeEvery(ROUTE_CHANGE_SUCCEED, requestCurrentContent),
+  ]);
 }
