7793f5df0d1294525edc32a99e18222cee723fe4
David Arenas <darensr@gmail.com>
2017-11-10 11:09:49 +0100

    Advances in the router model definition

--- src/pwa/stores/__tests__/router.test.js
+++ src/pwa/stores/__tests__/router.test.js
@@ -1,43 +1,93 @@
 import { getSnapshot } from 'mobx-state-tree';
-import { Router, Context, Item } from '../router';
-
-
-
-function router(initialState, action, payload) {
-  const store = Router.create({ ...initialState });
-  if (action) store[action](payload);
-  return getSnapshot(store);
-}
+import { Router, Item } from '../router';
 
 test('Router store should initiate empty', () => {
-  const { contexts, activeContext, selected } = router();
+  const { contexts, activeContext, selected } = getSnapshot(Router.create({}));
   expect(contexts).toEqual([]);
   expect(activeContext).toEqual(null);
   expect(selected).toEqual(null);
 });
 
-test('Context is populated appropriately', () => {
-
-  const selected = {
+test('An item of type Single is populated appropriately', () => {
+  const item = Item.create({
+    id: 123,
     route: 'single',
     singleType: 'post',
-    singleId: 123,
-  };
+    singleId: 1234,
+  });
+  expect(getSnapshot(item)).toEqual({
+    id: 123,
+    route: 'single',
+    singleType: 'post',
+    singleId: 1234,
+    isReady: false,
+  });
+});
 
-  const activeContext = {
-    selected,
-    items: [ selected ],
-    infinite: false,
-    options: null,
-  };
+test('An item of type List is populated appropriately', () => {
+  const item = Item.create({
+    id: 124,
+    route: 'list',
+  });
+  expect(getSnapshot(item)).toEqual({
+    id: 124,
+    isReady: false,
+    listId: 0,
+    listType: 'latest',
+    page: 0,
+    route: 'list',
+    singleType: 'post',
+  });
+});
 
+test('Context is populated appropriately', () => {
   const store = Router.create({
-    activeContext,
+    activeContext: 333,
     contexts: [
-      activeContext
+      {
+        id: 333,
+        selected: 123,
+        items: [
+          {
+            id: 123,
+            route: 'single',
+            singleType: 'post',
+            singleId: 1234,
+          },
+          {
+            id: 124,
+            route: 'list',
+          },
+        ],
+        infinite: false,
+        options: null,
+      },
     ],
     selected: null,
   });
 
-  expect(store.activeContext).toEqual(activeContext);
+  expect(getSnapshot(store.activeContext)).toEqual({
+    id: 333,
+    selected: 123,
+    items: [
+      {
+        id: 123,
+        route: 'single',
+        singleType: 'post',
+        singleId: 1234,
+        isReady: false,
+      },
+      {
+        id: 124,
+        isReady: false,
+        listId: 0,
+        listType: 'latest',
+        page: 0,
+        route: 'list',
+        singleType: 'post',
+      },
+    ],
+    infinite: false,
+    options: null,
+  });
 });
--- src/pwa/stores/router.js
+++ src/pwa/stores/router.js
@@ -3,6 +3,7 @@ import { types } from 'mobx-state-tree';
 export const Id = types.union(types.string, types.number);
 
 export const List = types.model('List').props({
+  id: types.identifier(Id),
   route: 'list',
   listType: types.optional(types.string, 'latest'),
   listId: types.optional(Id, 0),
@@ -13,6 +14,7 @@ export const List = types.model('List').props({
 });
 
 export const Single = types.model('Single').props({
+  id: types.identifier(Id),
   route: 'single',
   singleType: types.string,
   singleId: Id,
@@ -20,19 +22,18 @@ export const Single = types.model('Single').props({
   // goBack: types.reference(Context),
 });
 
-export const Item = types.union(List, Single);
+export const Item = types.union(({ route }) => (route === 'list' ? List : Single), List, Single);
 
 export const Context = types.model('Context').props({
+  id: types.identifier(Id),
   options: types.frozen,
   selected: types.reference(Item),
   items: types.array(types.union(Item, types.array(Item))),
   infinite: true,
-  // overwrite: false, --> this property might be used only when calling an action...
 });
+
 export const Router = types.model('Router').props({
   contexts: types.optional(types.array(Context), []),
   activeContext: types.optional(types.union(types.reference(Context), types.null), null),
   selected: types.optional(types.union(types.reference(Item), types.null), null),
 });
-
-// Item
