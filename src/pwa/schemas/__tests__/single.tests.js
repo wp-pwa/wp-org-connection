be780790ac9541c7459355661aa517728598bedd
Luis Herranz <luisherranz@gmail.com>
2017-11-17 12:48:34 +0100

    Refactor schemas to use single and list.

---
+++ src/pwa/schemas/__tests__/single.tests.js
@@ -0,0 +1,43 @@
+import { normalize } from 'normalizr';
+import post60 from './api/post-60.json';
+import category7 from './api/category-7.json';
+import tag10 from './api/tag-10.json';
+import author4 from './api/author-4.json';
+import { single } from '../';
+
+test('Convert post using single', () => {
+  const { entities } = normalize(post60, single);
+  expect(entities.post[60].id).toBe(60);
+  expect(entities.post[60].slug).toBe('the-beauties-of-gullfoss');
+  expect(entities.media[62].id).toBe(62);
+  expect(entities.media[62].slug).toBe('iceland-test');
+  expect(entities.author[4].id).toBe(4);
+  expect(entities.author[4].slug).toBe('alan');
+  expect(entities.taxonomy[3].id).toBe(3);
+  expect(entities.taxonomy[3].slug).toBe('photography');
+  expect(entities.taxonomy[3].taxonomy).toBe('category');
+  expect(entities.taxonomy[10].id).toBe(10);
+  expect(entities.taxonomy[10].slug).toBe('gullfoss');
+  expect(entities.taxonomy[10].taxonomy).toBe('post_tag');
+});
+
+test('Convert a category using single', () => {
+  const { entities } = normalize(category7, single);
+  expect(entities.taxonomy[7].id).toBe(7);
+  expect(entities.taxonomy[7].slug).toBe('nature');
+  expect(entities.taxonomy[7].taxonomy).toBe('category');
+});
+
+
+test('Convert a tag using single', () => {
+  const { entities } = normalize(tag10, single);
+  expect(entities.taxonomy[10].id).toBe(10);
+  expect(entities.taxonomy[10].slug).toBe('gullfoss');
+  expect(entities.taxonomy[10].taxonomy).toBe('post_tag');
+});
+
+test('Convert a author using single', () => {
+  const { entities } = normalize(author4, single);
+  expect(entities.author[4].id).toBe(4);
+  expect(entities.author[4].slug).toBe('alan');
+});
--- src/pwa/schemas/posts.js
+++ src/pwa/schemas/posts.js
@@ -4,7 +4,7 @@ const author = new schema.Entity('author');
 const media = new schema.Entity('media');
 const taxonomy = new schema.Entity('taxonomy');
 const taxonomies = new schema.Array(new schema.Array(taxonomy));
-export const post = new schema.Entity('post')
+export const post = new schema.Entity('post');
 post.define({
   _embedded: {
     author: [author],
@@ -12,5 +12,5 @@ post.define({
     'wp:term': taxonomies,
     up: post,
   },
-})
+});
 export const posts = new schema.Array(post);
