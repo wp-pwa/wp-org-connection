1610d1bd5087e4339b3e004cf384a385fa3815e9
Luis Herranz <luisherranz@gmail.com>
2017-11-18 11:28:58 +0100

    Converters ready

--- src/pwa/converters/index.js
+++ src/pwa/converters/index.js
@@ -1,7 +1,65 @@
-export default ({ entities }) => {
-  Object.entries(entities).forEach(([key, value]) => {
-    let type = 'single';
-    if (key === 'author') type = 'author';
-    
-  })
+/* eslint-disable no-underscore-dangle */
+export const post = entity => ({
+  id: entity.id,
+  type: entity.type,
+  creationDate: entity.date,
+  modificationDate: entity.modified,
+  title: entity.title.rendered,
+  slug: entity.slug,
+  link: entity.link,
+  content: entity.content.rendered,
+  excerpt: entity.excerpt.rendered,
+  author: entity.author,
+  featured: entity.featured_media,
+  taxonomiesMap: entity.taxonomiesMap,
+  meta: entity.meta,
+});
+
+export const taxonomy = entity => ({
+  id: entity.id,
+  name: entity.name,
+  slug: entity.slug,
+  link: entity.link,
+  taxonomy: entity.taxonomy,
+});
+
+export const author = entity => ({
+  id: entity.id,
+  name: entity.name,
+  slug: entity.slug,
+  description: entity.description,
+  link: entity.link,
+  avatar: entity.avatar_urls && Object.values(entity.avatar_urls)[0].replace(/\?.*$/, ''),
+});
+
+export const media = entity => ({
+  id: entity.id,
+  creationDate: entity.date,
+  slug: entity.slug,
+  alt: entity.alt_text,
+  mimeType: entity.mime_type,
+  mediaType: entity.media_type,
+  title: entity.title.rendered,
+  author: entity.author,
+  original: {
+    height: entity.media_details.height,
+    width: entity.media_details.width,
+    filename: entity.media_details.file,
+    url: entity.source_url,
+  },
+  sizes:
+    entity.media_details.sizes &&
+    Object.values(entity.media_details.sizes).map(image => ({
+      height: image.height,
+      width: image.width,
+      filename: image.file,
+      url: image.source_url,
+    })),
+});
+
+export default entity => {
+  if (entity.media_details) return media(entity);
+  else if (entity.taxonomy) return taxonomy(entity);
+  else if (entity.name) return author(entity);
+  return post(entity);
 };
