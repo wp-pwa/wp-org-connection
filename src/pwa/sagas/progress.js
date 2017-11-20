3c733153cd3a1f7e4d494a28b1303a5ea3f56936
Luis Herranz <luisherranz@gmail.com>
2017-11-20 13:42:45 +0100

    Use actionType from connection

--- src/pwa/sagas/progress.js
+++ src/pwa/sagas/progress.js
@@ -1,6 +1,7 @@
 import { take, all, fork, takeEvery } from 'redux-saga/effects';
 import { dep } from 'worona-deps';
 import NProgress from 'nprogress';
+import * as actionTypes from '../actionTypes';
 
 function* initProgress() {
   yield take(dep('build', 'actionTypes', 'CLIENT_RENDERED'));
@@ -16,6 +17,6 @@ function stopProgress() {
 export default function* progressSagas() {
   yield all([
     fork(initProgress),
-    takeEvery(dep('router', 'actionTypes', 'ROUTE_CHANGE_SUCCEED'), stopProgress),
+    takeEvery(actionTypes.ROUTE_CHANGE_SUCCEED, stopProgress),
   ]);
 }
