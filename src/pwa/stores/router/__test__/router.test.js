fd89ad0e94fad2994bbd4e1bc620b2e4b6eb0aa1
David Arenas <darensr@gmail.com>
2017-11-20 09:11:52 +0100

    WIP tests fixed for creating another context from selected

--- src/pwa/stores/router/__test__/router.test.js
+++ src/pwa/stores/router/__test__/router.test.js
@@ -1,8 +1,4 @@
 import { getSnapshot } from 'mobx-state-tree';
-import uuid from 'uuid/v4';
-import { Item } from '../item';
-import Column from '../column';
-import Context from '../context';
 import Router from '../router';
 
 import * as actionTypes from '../../../actionTypes'
@@ -49,23 +45,28 @@ test('Creates a new context from selected list', () => {
   expect(store.contexts[0].columns[0].selected.column._id).toBe(store.contexts[0].columns[0]._id);
 });
 
-test('Creates a new context from selected', () => {
+test('Creates another context from selected', () => {
   const store = Router.create();
   store[actionTypes.ROUTE_CHANGE_SUCCEED](actions.routeChangeSucceed({
     selected: { singleType: 'post', singleId: 60 },
   }))
 
+  expect(store.context.getItem({ singleType: 'post', singleId: 70 })).toBe(null);
+
   store[actionTypes.ROUTE_CHANGE_SUCCEED](actions.routeChangeSucceed({
     selected: { singleType: 'post', singleId: 70 },
   }))
 
-  expect(store.selected.type).toBe('post');
-  expect(store.selected.id).toBe(70);
+  // Current item
   expect(store.context.index).toBe(1);
+  expect(store.selected.id).toBe(70);
+  expect(store.selected.type).toBe('post');
   expect(store.context.column.selected.id).toBe(70);
   expect(store.context.columns[0].selected.id).toBe(70);
   expect(store.context.columns[0].items[0].id).toBe(70);
-  expect(store.contexts[0].columns[0].items[0].id).toBe(70);
-  expect(store.contexts[0].columns[0].selected.id).toBe(70);
-  expect(store.contexts[0].columns[0].selected.column._id).toBe(store.contexts[0].columns[0]._id);
+  expect(store.contexts[1].columns[0].items[0].id).toBe(70);
+  expect(store.contexts[1].columns[0].selected.id).toBe(70);
+  // Previous item
+  expect(store.contexts[0].columns[0].items[0].id).toBe(60);
+  expect(store.contexts[0].columns[0].selected.id).toBe(60);
 });
