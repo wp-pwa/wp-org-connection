37c9cc39486ae6e77c9be91d6f4159f21f1665be
Luis Herranz <luisherranz@gmail.com>
2017-11-20 12:59:03 +0100

    Export client sagas

--- src/pwa/index.js
+++ src/pwa/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import * as actions from './actions';
 import * as actionTypes from './actionTypes';
 import reducers from './reducers';
+import sagas from './sagas/client';
 // import * as components from './components';
-// import sagas from './sagas/client';
 // import selectors from './selectors';
 // import selectorCreators from './selectorCreators';
 
 const WpOrgConnection = () => <div>hi from wp-org</div>;
 
 export default WpOrgConnection;
-export { actions, actionTypes, reducers };
+export { actions, actionTypes, reducers, sagas };
--- src/pwa/sagas/progress.js
+++ src/pwa/sagas/progress.js
@@ -3,7 +3,7 @@ import { dep } from 'worona-deps';
 import NProgress from 'nprogress';
 
 function* initProgress() {
-  yield take(dep('build', 'types', 'CLIENT_REACT_RENDERED'));
+  yield take(dep('build', 'actionTypes', 'CLIENT_RENDERED'));
   NProgress.configure({ showSpinner: false });
 }
 
@@ -16,6 +16,6 @@ function stopProgress() {
 export default function* progressSagas() {
   yield all([
     fork(initProgress),
-    takeEvery(dep('router', 'types', 'ROUTE_CHANGE_SUCCEED'), stopProgress),
+    takeEvery(dep('router', 'actionTypes', 'ROUTE_CHANGE_SUCCEED'), stopProgress),
   ]);
 }
