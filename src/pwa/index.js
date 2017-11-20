d89d385d58a2fec7510541f9cd8fdf90a4659058
Luis Herranz <luisherranz@gmail.com>
2017-11-20 10:34:11 +0100

    Export actionTypes and remove reducers

--- src/pwa/index.js
+++ src/pwa/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import * as actions from './actions';
-import * as types from './types';
-import reducers from './reducers';
+import * as actionTypes from './actionTypes';
 // import * as components from './components';
 // import sagas from './sagas/client';
 // import selectors from './selectors';
@@ -10,4 +9,4 @@ import reducers from './reducers';
 const WpOrgConnection = () => <div>hi from wp-org</div>;
 
 export default WpOrgConnection;
-export { actions, types, reducers };
+export { actions, actionTypes };
