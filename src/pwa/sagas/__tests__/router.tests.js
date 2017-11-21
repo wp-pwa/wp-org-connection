e990ce51216fd27568e687951f406982bb3daecd
David Arenas <darensr@gmail.com>
2017-11-21 17:52:23 +0100

    WIP starting with router sagas implementation

--- src/pwa/sagas/__tests__/router.tests.js
+++ src/pwa/sagas/__tests__/router.tests.js
@@ -1,19 +1,63 @@
 import { expectSaga } from 'redux-saga-test-plan';
 import createHistory from 'history/createMemoryHistory';
 import routerSagaCreator from '../router.client';
+import * as actions from '../../actions';
 
 test('History gets replace with route from server', () => {
   const history = createHistory();
   const store = {
-    router: { selected: { type: 'latest', id: null, page: 1 }, context: { id: 0 } },
+    router: {
+      selected: {
+        listId: 'post',
+        listType: 'latest',
+        type: 'latest',
+        id: 'post',
+        page: 1,
+      },
+      context: { id: 0 },
+    },
   };
   const routerSaga = routerSagaCreator({ store, history });
   expectSaga(routerSaga).run();
-  expect(history.location.pathname).toBe('/latest/null/1/0');
+  expect(history.location.pathname).toBe('/latest/post/1/0');
   expect(history.location.state).toEqual({
-    type: 'latest',
-    id: null,
-    page: 1,
-    context: 0,
+    method: 'push',
+    selected: { listId: 'post', page: 1, listType: 'latest' },
+  });
+});
+
+test('Router saga attends route change requests', () => {
+  const history = createHistory();
+  const store = {
+    router: {
+      selected: {
+        listId: 'post',
+        listType: 'latest',
+        type: 'latest',
+        id: 'post',
+        page: 1,
+      },
+      context: { id: 0 },
+    },
+    connection: {
+      single: {
+        post: {
+          60: {
+            link: '/this-is-the-60th-post',
+          },
+        },
+      },
+    },
+  };
+  const routerSaga = routerSagaCreator({ store, history });
+  expectSaga(routerSaga)
+    .put(actions.routeChangeSucceed({ selected: { singleType: 'post', singleId: 60 } }))
+    .dispatch(actions.routeChangeRequested({ selected: { singleType: 'post', singleId: 60 } }))
+    .run();
+
+  expect(history.location.pathname).toBe('/this-is-the-60th-post');
+  expect(history.location.state).toEqual({
+    selected: { singleType: 'post', singleId: 60 },
+    method: 'push',
   });
 });
--- src/pwa/sagas/router.client.js
+++ src/pwa/sagas/router.client.js
@@ -15,21 +15,32 @@ const routeChanged = history =>
     return unlisten;
   });
 
-export function* handleHistoryRouteChange(changed) {
+export function* handleHistoryRouteChanges(changed) {
   yield put(actions.routeChangeSucceed(changed));
 }
 
+export const requestHandler = ({ store, history }) =>
+  function handleRequest({ selected, method, context }) {
+    const { type, id, page } = selected;
+
+    // get item
+    const { link } =
+      page !== undefined ? store.connection.single[type][id] : `/${type}/${id}/${page}/${context}`;
+
+    console.log(link);
+
+    if (['push', 'replace'].includes(method)) {
+      history[method](link, { selected, method, context });
+    }
+  };
+
 export default ({ store, history = createHistory() }) =>
   function* routerSaga() {
-    const { type, id, page } = store.router.selected;
+    const { type, id, listType, listId, singleType, singleId, page } = store.router.selected;
     const context = store.router.context.id;
     const { generator } = store.router.context;
     history.replace(`/${type}/${id}/${page}/${context}`, {
-      selected: {
-        type,
-        id,
-        page,
-      },
+      selected: { listType, listId, page, singleType, singleId },
       method: 'push',
       context: generator,
     });
@@ -38,9 +49,6 @@ export default ({ store, history = createHistory() }) =>
     const routeChangedEvents = routeChanged(history);
 
     // Track router events and dispatch them to redux.
-    yield takeEvery(routeChangedEvents, handleHistoryRouteChange);
-
-    // yield takeEvery(actionTypes.ROUTE_CHANGE_REQUESTED, requested => {
-    //   history.push(getPathname(requested), requested);
-    // });
+    yield takeEvery(routeChangedEvents, handleHistoryRouteChanges);
+    yield takeEvery(actionTypes.ROUTE_CHANGE_REQUESTED, requestHandler({ store, history }));
   };
