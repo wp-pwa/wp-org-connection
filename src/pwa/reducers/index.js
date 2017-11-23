799cc6e8445da6f710b4ac4d393d97064e4368c2
Luis Herranz <luisherranz@gmail.com>
2017-11-23 12:02:44 +0100

    Create a new store on each request (wp-pwa update required)

--- src/pwa/reducers/index.js
+++ src/pwa/reducers/index.js
@@ -1,25 +1,29 @@
-import { getSnapshot } from 'mobx-state-tree';
+import { applySnapshot, getSnapshot } from 'mobx-state-tree';
 import * as actionTypes from '../actionTypes';
-import Connection from '../stores';
 
-export const store = Connection.create({})
-
-export const reducers = () => (state, { type, ...action }) => {
-  switch (type) {
-    case actionTypes.SINGLE_REQUESTED:
-    case actionTypes.SINGLE_FAILED:
-    case actionTypes.SINGLE_SUCCEED:
-    case actionTypes.LIST_REQUESTED:
-    case actionTypes.LIST_SUCCEED:
-    case actionTypes.LIST_FAILED:
-    case actionTypes.CUSTOM_REQUESTED:
-    case actionTypes.CUSTOM_SUCCEED:
-    case actionTypes.CUSTOM_FAILED:
-    case actionTypes.ROUTE_CHANGE_SUCCEED:
-      store[type](action)
-      break;
-    default:
-      break;
+export default store => {
+  let firstTime = true;
+  return (state, { type, ...action }) => {
+    if (state !== undefined && firstTime) {
+      applySnapshot(store, state);
+      firstTime = false;
+    }
+    switch (type) {
+      case actionTypes.SINGLE_REQUESTED:
+      case actionTypes.SINGLE_FAILED:
+      case actionTypes.SINGLE_SUCCEED:
+      case actionTypes.LIST_REQUESTED:
+      case actionTypes.LIST_SUCCEED:
+      case actionTypes.LIST_FAILED:
+      case actionTypes.CUSTOM_REQUESTED:
+      case actionTypes.CUSTOM_SUCCEED:
+      case actionTypes.CUSTOM_FAILED:
+      case actionTypes.ROUTE_CHANGE_SUCCEED:
+        store[type](action)
+        break;
+      default:
+        break;
+    }
+    return getSnapshot(store);
   }
-  return getSnapshot(store);
 }
