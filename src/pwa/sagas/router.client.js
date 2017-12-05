d7d9f5968afdd3d9314047870c2759290170bb62
David Arenas <darensr@gmail.com>
2017-12-05 11:46:33 +0100

    WIP Router saga modified to set links properly

--- src/pwa/sagas/router.client.js
+++ src/pwa/sagas/router.client.js
@@ -1,5 +1,4 @@
 /* eslint-disable global-require */
-import { getType } from 'mobx-state-tree';
 import { when } from 'mobx';
 import { isMatch } from 'lodash';
 import { takeEvery, put, call } from 'redux-saga/effects';
@@ -7,116 +6,77 @@ import { eventChannel } from 'redux-saga';
 import createHistory from 'history/createBrowserHistory';
 import * as actionTypes from '../actionTypes';
 
-import { Media, Author, Taxonomy, Post } from '../stores/connection/single';
-
 import * as actions from '../actions';
 
-const routeChanged = history =>
+const getUrl = (selected, connection) => {
+  const { listType, listId, singleType, singleId, page } = selected;
+  const type = listType || singleType;
+  const id = listType ? listId : singleId;
+
+  let url;
+
+  if (type === 'latest') url = page > 1 ? `/page/${page}` : '/';
+  else {
+    const { link } = connection.single[type][id];
+    url = page > 1 ? link.paged(1) : link.url;
+  }
+
+  return url;
+}
+
+const routeChanged = ({ connection, history }) =>
   eventChannel(emitter => {
-    const unlisten = history.listen(location => {
+    const unlisten = history.listen((location, action) => {
       const { pathname, state } = location;
-      emitter({ pathname, ...state });
+
+      // Prevents an event emission when just replacing the URL
+      if (!(isMatch(connection.selected, state.selected) && action === 'REPLACE'))
+        emitter({ pathname, ...state });
     });
     return unlisten;
   });
 
 export function* handleHistoryRouteChanges(changed) {
-  // yield new Promise(resolve => setTimeout(resolve, 1000));
   yield put(actions.routeChangeSucceed(changed));
 }
 
-// const getUrl = (item, connection) => {
-//   const element = connection.single[item.type][item.id];
-//   const { link, type, id, slug, taxonomy } = element;
-//   const nodeType = getType(element);
-//
-//   if (link) return link;
-//
-//   // Entities with single
-//   if (nodeType === Post && type === 'page') return `/?page_id=${id}`;
-//   if (nodeType === Post) return `/?p=${id}`;
-//
-//   if (nodeType === Author) return `/?author=${id}`;
-//   if (nodeType === Media) return `/?attachement_id=${id}`; // does not work
-//
-//   if (nodeType === Taxonomy && taxonomy === 'category') return `/?cat=${id}`;
-//   if (nodeType === Taxonomy && taxonomy === 'tag') return `/?tag=${slug}`;
-//   if (nodeType === Taxonomy) return `/?${taxonomy}=${slug}`; // for custom taxonomies
-//
-//   // List (without single)
-//   if (type === 'archive') return `/?m=${id}`; // check this
-//   if (type === 'search') return `/?s=${id}`;
-//   if (type === 'latest') return `/?m=${id}`;
-//
-//   return '/';
-// };
-//
-// const getUrlPaged = (element, page, conn) =>
-//   element.link ? `${getUrl(element, conn)}/page/${page}` : `${getUrl(element, conn)}&paged=${page}`;
-
 export const requestHandlerCreator = ({ connection, history }) =>
   function* handleRequest({ selected, method, context }) {
-    const { listType, listId, singleType, singleId, page } = selected;
-
-    // get item
-    const link = listType
-      ? `/${listType}/${listId}/${page}/${context}`
-      : `/${singleType}/${singleId}/${context}`;
-    // : connection.connection.single[singleType][singleId].link;
-
-    // debugger;
-
+    const url = getUrl(selected, connection);
     if (['push', 'replace'].includes(method)) {
-      yield call(() => history[method](
-        // connection.context.getItem(selected).link.url,
-        link,
-        { selected, method, context },
-      ));
+      yield call(() => history[method](url, { selected, method, context }));
     }
   };
 
-// export const succeedHandlerCreator = ({ connection, history }) =>
-//   function* handleRequest({ selected }) {
-//     // yield call(() =>
-//     // when(
-//     //   () =>
-//     //     isMatch(connection.selected, selected) && connection.selected.entity,
-//     //   () => {
-//     //     console.log('WOTOWOTOW');
-//     //     const { state } = history.location;
-//     //     const { page } = state;
-//     //     const { entity } = connection.context.selected;
-//     //     // history.replace(page ? getUrlPaged(entity, page) : getUrl(entity), state);
-//     //     console.log(page ? getUrlPaged(entity, page) : getUrl(entity), state);
-//     //   },
-//     // );
-//     // );
-//
-//     const { state } = history.location;
-//     const { page } = state.selected;
-//     const { entity } = connection.context.selected;
-//     console.log(state, page, entity);
-//
-//     const sel = state.selected;
-//
-//     console.log(page && sel ? getUrlPaged(sel, page, connection) : getUrl(sel, connection), state);
-//
-//     yield call(() => console.log('SUCCEED'));
-//   };
+export const succeedHandlerCreator = ({ connection, history }) =>
+  function* handleRequest({ selected: { singleType, singleId, listType, listId, page } }) {
+    yield call(() => {
+      const type = listType || singleType;
+      const id = listType ? listId : singleId;
+
+      if (type === 'latest') return;
+
+      // Update value if it was not pretty before
+      const { link } = connection.single[type][id];
+      when(
+        () => link.pretty,
+        () => history.replace(page > 1 ? link.paged(1) : link.url, history.location.state),
+      );
+    });
+  };
 
 export default function* routerSaga(stores, history = createHistory()) {
   const { connection } = stores;
-  const { type, id, listType, listId, singleType, singleId, page } = connection.selected;
-  const context = connection.context.id;
+  const { selected } = connection;
   const { generator } = connection.context;
-  history.replace(`/${type}/${id}/${page}/${context}`, {
-    selected: { listType, listId, page, singleType, singleId },
+  history.replace(getUrl(connection.selected, connection), {
+    selected,
     method: 'push',
     context: generator,
   });
 
   // Initializate router event channels.
-  const routeChangedEvents = routeChanged(history);
+  const routeChangedEvents = routeChanged({ connection, history });
 
   // Track router events and dispatch them to redux.
   yield takeEvery(routeChangedEvents, handleHistoryRouteChanges);
@@ -124,5 +84,5 @@ export default function* routerSaga(stores, history = createHistory()) {
     actionTypes.ROUTE_CHANGE_REQUESTED,
     requestHandlerCreator({ connection, history }),
   );
-  // yield takeEvery(actionTypes.ROUTE_CHANGE_SUCCEED, succeedHandlerCreator({ connection, history }));
+  yield takeEvery(actionTypes.ROUTE_CHANGE_SUCCEED, succeedHandlerCreator({ connection, history }));
 }
