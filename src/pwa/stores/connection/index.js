374ee19da8e89ed734fd9056da9f8421e852d848
Luis Herranz <luisherranz@gmail.com>
2018-02-05 16:49:48 +0100

    Tests for single and list actions

--- src/pwa/stores/connection/index.js
+++ src/pwa/stores/connection/index.js
@@ -86,68 +86,25 @@ export const actions = self => ({
       if (total.pages) list.total.pages = total.pages;
     }
   },
-  [actionTypes.SINGLE_REQUESTED]({ entityType, entityId }) {
-    // self.single(entityType, entityId).fetching = true;
-  },
-  [actionTypes.SINGLE_FAILED]({ entityType, entityId }) {
-    // self.single(entityType, entityId).fetching = false;
+  [actionTypes.SINGLE_REQUESTED]({ singleType: type, singleId: id }) {
+    self.fetchingEntity({ type, id });
   },
   [actionTypes.SINGLE_SUCCEED]({ entities }) {
-    // self.addEntities({ entities, ready: true, fetching: false });
+    self.addEntities({ entities });
   },
-  [actionTypes.LIST_REQUESTED]({ listType, listId, page }) {
-    // self.list(listType, listId).fetching = true;
-    // self.list(listType, listId).page(page);
-    // init({ self, listType, listId, page, fetching: true });
+  [actionTypes.SINGLE_FAILED]({ singleType: type, singleId: id }) {
+    const item = self.getEntity({ type, id });
+    item.fetching = false;
+  },
+  [actionTypes.LIST_REQUESTED]({ listType: type, listId: id, page = 1 }) {
+    self.fetchingPage({ type, id, page });
   },
   [actionTypes.LIST_SUCCEED]({ listType, listId, page, total, result, entities }) {
-    // Update the list.
-    // const list = self.listMap.get(listType).get(listId);
-    // list.fetching = false;
-    // list.ready = true;
-    // list.pageMap.get(page - 1).fetching = false;
-    // list.pageMap.get(page - 1).ready = true;
-    // list.pageMap.get(page - 1).entities = result;
-    // if (total) list.total = total;
-    //
-    // addEntities({ self, entities, ready: true, fetching: false });
-    // if (self.context) extractList({ listType, listId, page, result }, self.context);
+    self.addPage({ type: listType, id: listId, page, total, result, entities });
+    if (self.context) extractList({ listType, listId, page, result }, self.context);
   },
-  [actionTypes.LIST_FAILED]({ listType, listId, page }) {
-    // Populate the state with the entity value and set both fetching and ready.
-    // self.listMap.get(listType).get(listId).fetching = false;
-    // self.listMap
-    //   .get(listType)
-    //   .get(listId)
-    //   .pageMap.get(page - 1).fetching = false;
-  },
-  [actionTypes.CUSTOM_REQUESTED]({ url, params, name, page }) {
-    // if (!self.customMap.get(name)) self.customMap.set(name, {});
-    // const custom = self.customMap.get(name);
-    // custom.fetching = true;
-    // custom.url = url;
-    // custom.params = params;
-    // if (!custom.pageMap.get(page - 1)) custom.pageMap.set(page - 1, {});
-    // custom.pageMap.get(page - 1).fetching = true;
-  },
-  [actionTypes.CUSTOM_SUCCEED]({ name, page, total, result, entities }) {
-    // const custom = self.customMap.get(name);
-    // custom.fetching = false;
-    // custom.ready = true;
-    // custom.pageMap.get(page - 1).fetching = false;
-    // custom.pageMap.get(page - 1).ready = true;
-    // custom.pageMap.get(page - 1).entities = result;
-    // custom.total = total;
-    // addEntities({ self, entities, ready: true, fetching: false });
-  },
-  [actionTypes.CUSTOM_FAILED]({ name, page }) {
-    // const custom = self.customMap.get(name);
-    // custom.fetching = false;
-    // custom.pageMap.get(page - 1).fetching = false;
-  },
-  [actionTypes.SITE_INFO_SUCCEED]({ home: { title, description }, perPage }) {
-    // self.siteInfo.home.title = title;
-    // self.siteInfo.home.description = description;
-    // self.siteInfo.perPage = perPage;
+  [actionTypes.LIST_FAILED]({ listType: type, listId: id, page = 1 }) {
+    const item = self.getPage({ type, id, page });
+    item.fetching = false;
   },
 });
