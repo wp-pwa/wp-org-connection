4bd91486f24029d96bb365dfb9d8e5d2cc7db234
Luis Herranz <luisherranz@gmail.com>
2018-02-05 14:00:02 +0100

    Several pages working

--- src/pwa/stores/connection/__tests__/list.tests.js
+++ src/pwa/stores/connection/__tests__/list.tests.js
@@ -5,9 +5,15 @@ import { normalize } from 'normalizr';
 import * as connect from '../';
 import { list } from '../../../schemas';
 import postsFromCategory7 from '../../../__tests__/posts-from-category-7.json';
+import postsFromCategory7Page2 from '../../../__tests__/posts-from-category-7-page-2.json';
 
 const { result: resultFromCategory7, entities: entitiesFromCategory } = normalize(
   postsFromCategory7,
+  list
+);
+
+const { result: resultFromCategory7Page2, entities: entitiesFromCategoryPage2 } = normalize(
+  postsFromCategory7Page2,
   list
 );
 
@@ -51,7 +57,7 @@ describe('Store › List', () => {
     connection.fetchingList({ type: 'category', id: 7 });
   });
 
-  test('Get list entity shapes after addListPage', () => {
+  test('Get list entity shapes after adding a page', () => {
     expect(connection.list('category', 7).entities).toEqual([]);
     expect(connection.list('category', 7).page(1).entities).toEqual([]);
     connection.addListPage({
@@ -61,7 +67,7 @@ describe('Store › List', () => {
       result: resultFromCategory7,
       entities: entitiesFromCategory
     });
-    const results = [57, 54, 42, 36, 33, 30, 26, 23, 12, 1];
+    const results = [57, 54, 42, 36, 33];
     expect(connection.list('category', 7).entities.map(entity => entity.id)).toEqual(results);
     expect(
       connection
@@ -69,6 +75,51 @@ describe('Store › List', () => {
         .page(1)
         .entities.map(entity => entity.id)
     ).toEqual(results);
+    expect(connection.list('category', 7).entities[0].id).toBe(57);
+    expect(connection.list('category', 7).entities[0].title).toBe('Shinjuku Gyoen National Garden');
+    expect(connection.list('category', 7).page(1).entities[0].id).toBe(57);
+    expect(connection.list('category', 7).page(1).entities[0].title).toBe(
+      'Shinjuku Gyoen National Garden'
+    );
+    expect(connection.list('category', 7).pages[0].entities[0].id).toBe(57);
+    expect(connection.list('category', 7).pages[0].entities[0].title).toBe(
+      'Shinjuku Gyoen National Garden'
+    );
+  });
+
+  test('Get list entity shapes after adding two pages', () => {
+    connection.addListPage({
+      type: 'category',
+      id: 7,
+      page: 1,
+      result: resultFromCategory7,
+      entities: entitiesFromCategory
+    });
+    connection.addListPage({
+      type: 'category',
+      id: 7,
+      page: 2,
+      result: resultFromCategory7Page2,
+      entities: entitiesFromCategoryPage2
+    });
+    const page1 = [57, 54, 42, 36, 33];
+    const page2 = [30, 26, 23, 12, 1];
+    expect(connection.list('category', 7).entities.map(entity => entity.id)).toEqual([
+      ...page1,
+      ...page2
+    ]);
+    expect(
+      connection
+        .list('category', 7)
+        .page(2)
+        .entities.map(entity => entity.id)
+    ).toEqual(page2);
+    expect(connection.list('category', 7).entities[5].id).toBe(30);
+    expect(connection.list('category', 7).entities[5].title).toBe('Machu Picchu');
+    expect(connection.list('category', 7).page(2).entities[0].id).toBe(30);
+    expect(connection.list('category', 7).page(2).entities[0].title).toBe('Machu Picchu');
+    expect(connection.list('category', 7).pages[1].entities[0].id).toBe(30);
+    expect(connection.list('category', 7).pages[1].entities[0].title).toBe('Machu Picchu');
   });
 
   test.skip('Subscribe to ready before entity is ready', done => {
