aefccb76dec897dd147515a33575e3723cbe81f4
David Arenas <darensr@gmail.com>
2018-02-08 14:09:27 +0100

    RouteWaypoint component working

--- src/pwa/components/RouteWaypoint/index.js
+++ src/pwa/components/RouteWaypoint/index.js
@@ -1,40 +1,78 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Waypoint from 'react-waypoint';
 import { connect } from 'react-redux';
 import { inject } from 'mobx-react';
 import { isMatch, noop } from 'lodash';
+import { dep } from 'worona-deps';
 import { routeChangeRequested } from '../../actions';
 
-const RouteWaypoint = ({ children, active, changeRoute }) => (
-  <Waypoint
-    onEnter={active ? changeRoute : noop}
-    topOffset="100px"
-    bottomOffset="100px"
-    scrollableAncestor="window"
-  >
-    {children}
-  </Waypoint>
-);
+class RouteWaypoint extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    ssr: PropTypes.bool.isRequired,
+    active: PropTypes.bool.isRequired,
+    changeRoute: PropTypes.func.isRequired,
+  };
 
-RouteWaypoint.propTypes = {
-  children: PropTypes.node.isRequired,
-  active: PropTypes.bool.isRequired,
-  changeRoute: PropTypes.func.isRequired,
-};
+  constructor(props) {
+    super(props);
+    this.changeRouteFromBelow = this.changeRouteFromBelow.bind(this);
+    this.changeRouteFromAbove = this.changeRouteFromAbove.bind(this);
+  }
+
+  changeRouteFromBelow({ previousPosition }) {
+    const { changeRoute } = this.props;
+    if (previousPosition === Waypoint.below) changeRoute();
+  }
+
+  changeRouteFromAbove({ previousPosition }) {
+    const { changeRoute } = this.props;
+    if (previousPosition === Waypoint.above) changeRoute();
+  }
+
+  render() {
+    const { children, ssr, active } = this.props;
+    return !ssr ? (
+      <Fragment>
+        <Waypoint
+          onEnter={active ? this.changeRouteFromBelow : noop}
+          topOffset={0}
+          bottomOffset={600}
+          scrollableAncestor={window}
+        />
+        {children}
+        <Waypoint
+          onEnter={active ? this.changeRouteFromAbove : noop}
+          topOffset={600}
+          bottomOffset={0}
+          scrollableAncestor={window}
+        />
+      </Fragment>
+    ) : (
+      children
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  ssr: dep('build', 'selectors', 'getSsr')(state),
+});
 
 const mapDispatchToProps = (dispatch, { selected, currentSelected, method }) => ({
   changeRoute() {
-    if (!isMatch(selected, currentSelected)) routeChangeRequested({ selected, method });
+    setTimeout(() => {
+      if (!isMatch(currentSelected, selected)) {
+        dispatch(routeChangeRequested({ selected, method }));
+      }
+    })
   },
 });
 
-export default connect(undefined, mapDispatchToProps)(
-  inject(({ connection }, { selected }) => {
-    const { context, selected: currentSelected } = connection;
-    const selectedColumn = context.getItem(selected).column;
-    const method = selectedColumn === currentSelected.column ? 'replace' : 'push';
-    const active = selectedColumn === context.column;
-    return { currentSelected, method, active };
-  })(RouteWaypoint),
-);
+export default inject(({ connection }, { selected }) => {
+  const { context, selected: currentSelected } = connection;
+  const selectedColumn = context.getItem(selected).column;
+  const method = selectedColumn === currentSelected.column ? 'replace' : 'push';
+  const active = selectedColumn === context.column;
+  return { currentSelected, method, active };
+})(connect(mapStateToProps, mapDispatchToProps)(RouteWaypoint));
