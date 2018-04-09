842fd8a479ad033fb56e54f78535340bd1c452e2
Luis Herranz <luisherranz@gmail.com>
2018-04-09 11:48:33 +0200

    Fix site info

--- src/pwa/sagas/server.js
+++ src/pwa/sagas/server.js
@@ -1,4 +1,5 @@
-import { spawn, put, all } from 'redux-saga/effects';
+import { spawn, put, all, take } from 'redux-saga/effects';
+import { CONNECTION_INITIALIZED } from '../actionTypes';
 import wpApiWatchers from './wp-api-watchers';
 import headContentWatcher from './headContent';
 import * as actions from '../actions';
@@ -8,6 +9,8 @@ export default function* wpOrgConnectionServerSaga({ stores }) {
   // Spawn the wp api watchers. We do not fork, so this doesn't block the saga.
   yield spawn(wpApiWatchers, stores);
   yield spawn(headContentWatcher);
+  // Wait until connection is initialized.
+  yield take(CONNECTION_INITIALIZED);
   // Request Site Info.
   yield put(actions.siteInfoRequested());
   // Request Head elements.
