74b5004b4c6fa1605e57dd8f334d788bfc5c255a
David Arenas <darensr@gmail.com>
2018-04-18 11:39:05 +0200

    Do not import reducers in amp/index.js

--- src/amp/index.js
+++ src/amp/index.js
@@ -1,8 +1,7 @@
 import * as actions from '../pwa/actions';
 import * as actionTypes from '../pwa/actionTypes';
 import WpOrgConnection, * as components from '../pwa/components';
-import reducers from '../pwa/reducers';
 import Store from '../pwa/stores';
 
 export default WpOrgConnection;
-export { actions, actionTypes, components, reducers, Store };
+export { actions, actionTypes, components, Store };
