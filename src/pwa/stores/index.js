ed56998a18590eb5c7f41af9f07181f157e3fa6c
Luis Herranz <luisherranz@gmail.com>
2018-04-23 18:25:30 +0200

    MST server sagas (WIP)

--- src/pwa/stores/index.js
+++ src/pwa/stores/index.js
@@ -1,7 +1,9 @@
-import { types, getEnv } from 'mobx-state-tree';
+import { types, getEnv, flow } from 'mobx-state-tree';
 import * as connection from './connection';
 import * as router from './router';
 import * as history from './router/history';
+
+
 
 const Connection = types
   .model('Connection')
@@ -13,8 +15,16 @@ const Connection = types
   .actions(router.actions)
   .actions(history.actions)
   .actions(self => ({
+    server: flow(function* ServerConnection() {
+      const asciify = eval('require("asciify")');
+      const asciifyPromise = txt => new Promise((resolve, reject) => {
+        asciify(txt, (err, res) => { if (err) reject(err); else resolve(res) });
+      });
+      const awesome = yield asciifyPromise('Frontity!');
+      console.log(awesome);
+    }),
     afterCreate: () => {
-      const { store } = getEnv(self);
+      const { store, isServer } = getEnv(self);
       if (store)
         store.subscribe(() => {
           const action = store.getState().lastAction;
