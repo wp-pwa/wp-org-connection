77e62321297d8312d7ea4b812d0f8aafa2ff0041
Luis Herranz <luisherranz@gmail.com>
2018-05-16 16:45:04 +0200

    Fix flows

--- src/pwa/client.js
+++ src/pwa/client.js
@@ -1,8 +1,8 @@
 import WpOrgConnection, * as components from './components';
-import stores from './stores';
-import flow from './flows/server';
+import Store from './stores';
+import flow from './flows/client';
 import env from './env';
 import { version } from '../../package.json';
 
 export default WpOrgConnection;
-export { components, stores, flow, env, version };
+export { components, Store, flow, env, version };
--- src/pwa/flows/client.js
+++ src/pwa/flows/client.js
@@ -1,1 +1,5 @@
-export default {}
+import { flow  } from 'mobx-state-tree';
+
+export default self => flow(function* () {
+  console.log('hi from client flow');
+})
