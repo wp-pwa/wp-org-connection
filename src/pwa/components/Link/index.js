6d25c9cb8c7fb5c197ca7bd0d91a080f788245a5
David Arenas <darensr@gmail.com>
2018-06-05 18:05:28 +0200

    Removes redux from Link component

--- src/pwa/components/Link/index.js
+++ src/pwa/components/Link/index.js
@@ -2,10 +2,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import NProgress from 'nprogress';
-import { compose } from 'recompose';
 import { inject } from 'mobx-react';
-import { connect } from 'react-redux';
-import { dep } from 'worona-deps';
 import { injectGlobal } from 'react-emotion';
 
 // eslint-disable-next-line
@@ -148,14 +145,7 @@ class Link extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  routeChangeRequested: payload =>
-    dispatch(dep('connection', 'actions', 'routeChangeRequested')(payload)),
-});
-
-export default compose(
-  inject(({ connection }, { type, id, page }) => ({
-    href: page ? connection.entity(type, id).pagedLink(page) : connection.entity(type, id).link,
-  })),
-  connect(null, mapDispatchToProps),
-)(Link);
+export default inject(({ connection }, { type, id, page }) => ({
+  href: page ? connection.entity(type, id).pagedLink(page) : connection.entity(type, id).link,
+  routeChangeRequested: connection.routeChangeSucceed,
+}))(Link);
