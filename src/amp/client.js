840a08831a157a60f74ac8c668060e6aead3424a
Eduardo Campaña <asrielo@gmail.com>
2018-06-28 18:00:50 +0200

    fix(amp): fixes client entry points in AMP
    fix https://github.com/wp-pwa/saturn-theme/issues/318

--- src/amp/client.js
+++ src/amp/client.js
@@ -1,1 +1,8 @@
-export default () => null;
+import Store from '../pwa/stores';
+import env from '../pwa/env';
+import { version } from '../../package.json';
+
+const WpOrgConnection = () => null;
+
+export default WpOrgConnection;
+export { Store, env, version };
--- src/amp/server.js
+++ src/amp/server.js
@@ -1,6 +1,8 @@
 import Store from '../pwa/stores';
+import env from '../pwa/env';
+import { version } from '../../package.json';
 
 const WpOrgConnection = () => null;
 
 export default WpOrgConnection;
-export { Store };
+export { Store, env, version };
