4dbe93396be35debf04aa94db6aeb0ac3b84d06d
Eduardo Campaña <asrielo@gmail.com>
2018-07-04 17:32:30 +0200

    feat(stores): removes flows and adds afterCsr hook
    fix https://github.com/wp-pwa/saturn-theme/issues/329

--- src/pwa/client.js
+++ src/pwa/client.js
@@ -1,9 +1,8 @@
 import Store from './stores';
-import flow from './flows/client';
 import env from './env';
 import { version } from '../../package.json';
 
 const WpOrgConnection = () => null;
 
 export default WpOrgConnection;
-export { Store, flow, env, version };
+export { Store, env, version };
