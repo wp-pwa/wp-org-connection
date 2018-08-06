1a2d6900cbc3336553f73217876152b247cca589
Luis Herranz <luisherranz@gmail.com>
2018-08-06 17:19:48 +0200

    refactor(head-meta): getEntityTitle function doesnt need to be created
    for each HeadMeta instance

--- src/pwa/stores/connection/head-meta.js
+++ src/pwa/stores/connection/head-meta.js
@@ -2,37 +2,34 @@ import { types, getParent, getEnv } from 'mobx-state-tree';
 import { isMatch } from 'lodash';
 import { decode } from 'he';
 
-export default types.model('HeadMeta').views(self => {
-  const getEntityTitle = entity =>
-    decode(
-      (entity.raw.yoast_meta && entity.raw.yoast_meta.title) ||
-        entity.raw.name ||
-        entity.raw.title.rendered,
-    ).replace(/<\/?[^>]+(>|$)/g, '');
+const getEntityTitle = entity =>
+  decode(
+    (entity.raw.yoast_meta && entity.raw.yoast_meta.title) ||
+      entity.raw.name ||
+      entity.raw.title.rendered,
+  ).replace(/<\/?[^>]+(>|$)/g, '');
 
-  return {
-    get title() {
-      const { initialSelectedItem: initial } = getEnv(self);
-      const { head } = getParent(self, 3);
-      const entity = getParent(self);
+export default types.model('HeadMeta').views(self => ({
+  get title() {
+    const { initialSelectedItem: initial } = getEnv(self);
+    const { head } = getParent(self, 3);
+    const entity = getParent(self);
 
-      return head.title &&
-        isMatch(entity, { type: initial.type, id: initial.id })
-        ? head.title
-        : getEntityTitle(entity);
-    },
-    pagedTitle(page) {
-      if (!page) return self.title;
+    return head.title && isMatch(entity, { type: initial.type, id: initial.id })
+      ? head.title
+      : getEntityTitle(entity);
+  },
+  pagedTitle(page) {
+    if (!page) return self.title;
 
-      const { initialSelectedItem: initial } = getEnv(self);
-      const { head } = getParent(self, 3);
-      const entity = getParent(self);
+    const { initialSelectedItem: initial } = getEnv(self);
+    const { head } = getParent(self, 3);
+    const entity = getParent(self);
 
-      return head.title &&
-        isMatch(entity, { type: initial.type, id: initial.id }) &&
-        page === initial.page
-        ? head.title
-        : getEntityTitle(entity);
-    },
-  };
-});
+    return head.title &&
+      isMatch(entity, { type: initial.type, id: initial.id }) &&
+      page === initial.page
+      ? head.title
+      : getEntityTitle(entity);
+  },
+}));
